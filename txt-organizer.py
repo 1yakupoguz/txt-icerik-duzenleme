6eb82eea654da6d70bb9bb977e5c302c5eec7f18
Yakup OĞUZ <95951188+1yakupoguz@users.noreply.github.com>
2023-11-19 19:36:01 +0300

    Add files via upload

---
+++ txt-organizer.py
@@ -0,0 +1,26 @@
+import os
+
+degistirilecekTxtlerinYolu="C:\\Users\\Yakup\\Resimler\\Yeniklasor\\"
+kaclaDegistirilecek='22'
+
+def degistirmeFonksiyonu(yol,data):
+    if not os.path.exists(yol):
+        raise FileNotFoundError("Geçersiz yol")
+    if not isinstance(kaclaDegistirilecek, str):
+        raise TypeError("kaclaDegistirilecek değişkenini tanımlarken tırnak koyunuz. Örnek kaclaDegistirilecek='22'")
+    if not 0 <= int(kaclaDegistirilecek) <= 21:
+        raise ValueError("kaclaDegistirilecek değişkeni 0 ile 21 aralığında olmalıdır")
+    else:
+        for (dirname, dirs, files) in os.walk(yol):
+            for filename in files:
+                if filename.endswith('.txt'):
+                    with open(yol+filename,"r") as dosya:
+                        dosyaIcerigi = dosya.readlines()
+                        with open(yol+filename,"w") as dosya:
+                            for icerik in dosyaIcerigi: 
+                                if icerik.startswith("0 "):
+                                    icerik=icerik.replace("0 ",data+" ",2)
+                                dosya.write(icerik)
+
+
+degistirmeFonksiyonu(degistirilecekTxtlerinYolu,kaclaDegistirilecek)
